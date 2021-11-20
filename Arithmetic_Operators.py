3e644f2540ce15e68efcbd9401cc41002089aa00
Praveen jha <Praveenjhafbg54321@gmail.com>
2021-11-20 19:06:41 +0530

    Arithmetic-Operators

---
+++ Arithmetic_Operators.py
@@ -0,0 +1,17 @@
+a = 10
+b = 7
+print("addition of 5 and 7 is = ",a+b)
+
+print("Subtraction of 5 and 7 is = ",a-b)
+
+print("Multiplication of ",a," and ",b," is = ",a*b)
+
+print("Division of ",a," and ",b," is = ",a/b)
+
+print("Modulus of ",a," is = " ,a%2,)
+
+print("Modulus of ",b," is = ",b%2)
+
+print("Exponentiation of ",a," is = ",a**3)     # it will print 10*10*10 = 1000
+
+print("Floor division of ",a," is = ",a//3)     # it will print the quotient when first operand is devided by the second = 3.

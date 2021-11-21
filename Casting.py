0f007ec3ff05ac45e2b19960b0f97edd01a7b11a
Praveen jha <Praveenjhafbg54321@gmail.com>
2021-11-21 22:44:49 +0530

    Casting

---
+++ Casting.py
@@ -0,0 +1,34 @@
+# Integers
+
+x = int(1)      # x will be 1
+y = int(2.8)    # y will  be 2
+z = int("3")    # z will be 3
+
+print("Converting ",x," into int ---->",x)
+print("Converting 2.8 into int----->",y)
+print('Converting "3" into int------>',z)
+print()
+
+# Float
+
+x1 = float(1)        # x will be 1.0
+y1 = float(2.8)      # y1 will be 2.8
+z1 = float("3")      # z1 will be 3.0
+w1 = float("4.2")    # w1 will be 4.2
+
+print("Converting 1 into float----->",x1)
+print("Converting 2.8 into float ----->",y1)
+print('Converting "3" into float------>',z1)
+print('Converting "4.2" itno float------>',w1)
+print()
+
+# String
+
+x2 = str("s1")      # x2 will be s1
+y2 = str(2)         # y2 will be '2'
+z2 = str(3.0)      # z2 will be '3.0'
+
+print('Converting "s1" itno String---->',x2)
+print("Converting 2 into String---->",y2)
+print("Converting 3.0 into String---->",z2)
+print()
